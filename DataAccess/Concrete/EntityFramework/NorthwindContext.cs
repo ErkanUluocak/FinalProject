4fc808c9f172596155f251621931828cacfc4830
Serkan Uluocak <Serkan Uluocak@DESKTOP-1OPU8PK>
2022-04-24 14:49:18 +0300

    son

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını ilişkilendirmek için kullandığımız yapı.
+    public class NorthwindContext : DbContext
+    {
+
+    }
+}
